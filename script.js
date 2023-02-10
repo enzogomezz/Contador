f0016bdd8251d2bcc3a98c250fb09d3fd668651b
enzogomezz <110132900+enzogomezz@users.noreply.github.com>
2023-02-10 19:06:08 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function biloto() {
+    let inicio = document.querySelector("input#inicio")
+    let passo = document.querySelector("input#passo")
+    let fim = document.querySelector("input#fim")
+    let res = document.querySelector("p#res")
+    let contador = Number((inicio).value)
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        res.innerHTML = `[ERRO] Impossível contar`
+        return
+    }
+    else if (fim.value > inicio.value) {
+        res.innerHTML = `Contando: `
+        do {
+            res.innerHTML += `${contador} \u{1F449}&nbsp;`
+            contador += Number((passo).value)
+        } while (contador <= Number((fim).value))
+        res.innerHTML += `\u{1F3C1}`
+    } else {
+        res.innerHTML = `Contando: `
+        do {
+            res.innerHTML += `${contador} \u{1F449}&nbsp;`
+            contador -= Number((passo).value)
+        } while (contador >= Number((fim).value))
+        res.innerHTML += `\u{1F3C1}`
+    }
+}
